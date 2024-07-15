d935977b8c9b169b63ca6c0a737b988d51c9756b
GOvEy1nw <45126527+GOvEy1nw@users.noreply.github.com>
2024-07-15 22:12:54 +0100

    fix

--- src/components/Layout/Sidebar/index.tsx
+++ src/components/Layout/Sidebar/index.tsx
@@ -224,7 +224,7 @@ const Sidebar = ({ open, setClosed }: SidebarProps) => {
               <div className="flex flex-shrink-0 items-center">
                 <span className="px-4 text-2xl text-gray-50">
                   <a href="/">
-                    <img src="/logo_full.webp" alt="Logo" style={{ width: '100%', height: 'auto' }} />
+                    <img src="/logo_rais.webp" alt="Logo" style={{ width: '100%', height: 'auto' }} />
                   </a>
                 </span>
               </div>
